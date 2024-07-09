790726542624064f00273cc3940c255745fb119b
Daniel31513 <123525969+Daniel31513@users.noreply.github.com>
2024-07-09 07:36:15 -0600

    avance js

--- encrip.js
+++ encrip.js
@@ -1,6 +1,23 @@
 function encriptar(){
 
-    let mensage=document.getElementById("encr").value;
+    let mensagee=document.getElementById("encr").value;
 
+    let codificado=mensagee.replace(/a/g,"enter")
+        codificado=mensagee.replace(/e/g,"imes")
+        codificado=mensagee.replace(/i/g,"ai")
+        codificado=mensagee.replace(/o/g,"ober")
+        codificado=mensagee.replace(/u/g,"ufat")
+
+    
+}
+
+function desencriptar(){
+    let mensajed=document.getElementById("desencr").value;
+
+    let decodificado=mensajed.replace(/enter/g,"a")
+        decodificado=mensajed.replace(/imes/g,"e")
+        decodificado=mensajed.replace(/ai/g,"i")
+        decodificado=mensajed.replace(/ober/g,"o")
+        decodificado=mensajed.replace(/ufat/g,"u");
     
 }
